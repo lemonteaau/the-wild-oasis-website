00d1d45342bdfa893f0bc13238f8eec014a871b2
Terry Cheng <cyr062599@gmail.com>
2024-07-01 19:21:38 +0930

    refactor: Update layout.js to use grid for responsive layout

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,11 @@
+export const metadata = {
+  title: "Guest area",
+};
+
 export default function Page() {
   return (
-    <div>
-      <h1>Your Account</h1>
-    </div>
+    <h2 className="mb-7 text-2xl font-semibold text-accent-400">
+      Welcome, XIJINPING!
+    </h2>
   );
 }
