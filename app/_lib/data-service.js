8025adc6b81afe09a525d96c6b598e560e502cbd
Terry Cheng <cyr062599@gmail.com>
2024-07-03 16:52:57 +0930

    refactor: Improve loading experience with lazy loading for images

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -12,7 +12,7 @@ export async function getCabin(id) {
     .single();
 
   // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
+  // await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
@@ -130,6 +130,8 @@ export async function getBookedDatesByCabinId(cabinId) {
 
 export async function getSettings() {
   const { data, error } = await supabase.from("settings").select("*").single();
+
+  // await new Promise((res) => setTimeout(res, 5000));
 
   if (error) {
     console.error(error);
