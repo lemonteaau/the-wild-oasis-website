baa95474ce43b8841c7935a98a798cc353a2b937
Terry Cheng <cyr062599@gmail.com>
2024-07-03 11:55:31 +0930

    chore: Update

--- app/account/page.js
+++ app/account/page.js
@@ -5,7 +5,7 @@ export const metadata = {
 export default function Page() {
   return (
     <h2 className="mb-7 text-2xl font-semibold text-accent-400">
-      Welcome, XIJINPING!
+      Welcome, guestName!
     </h2>
   );
 }
