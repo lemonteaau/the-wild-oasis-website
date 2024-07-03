bc9b8a5977f3778d69caf086be074725c1ce21e6
Terry Cheng <cyr062599@gmail.com>
2024-07-03 15:57:29 +0930

    refactor: Add filter functionality to CabinList component

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -2,7 +2,7 @@ import { unstable_noStore as noStore } from "next/cache";
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // noStore();
 
   const cabins = await getCabins();
@@ -11,9 +11,25 @@ async function CabinList() {
     return null;
   }
 
+  let displayedCabins;
+  if (filter === "all") {
+    displayedCabins = cabins;
+  }
+  if (filter === "small") {
+    displayedCabins = cabins.filter((cabins) => cabins.maxCapacity <= 3);
+  }
+  if (filter === "medium") {
+    displayedCabins = cabins.filter(
+      (cabins) => cabins.maxCapacity >= 4 && cabins.maxCapacity <= 7,
+    );
+  }
+  if (filter === "large") {
+    displayedCabins = cabins.filter((cabins) => cabins.maxCapacity >= 8);
+  }
+
   return (
     <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
