931647aa582794c079419d375097465b3dfdd23e
Terry Cheng <cyr062599@gmail.com>
2024-07-07 16:40:34 +0930

    refactor: Update Page component to use async/await for authentication and display user's first name

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ")[0];
+
   return (
     <h2 className="mb-7 text-2xl font-semibold text-accent-400">
-      Welcome, guestName!
+      Welcome, {firstName}!
     </h2>
   );
 }
