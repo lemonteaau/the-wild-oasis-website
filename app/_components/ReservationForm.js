e27168e73619910a00e3f3d78e7a85fb7267f83d
Terry Cheng <cyr062599@gmail.com>
2024-07-07 15:05:18 +0930

    refactor: Update DateSelector component to use ReservationContext for managing reservation state

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,5 +1,9 @@
+"use client";
+
+import { useReservation } from "./ReservationContext";
+
 function ReservationForm({ cabin }) {
-  // CHANGE
+  const { range } = useReservation();
   const { maxCapacity } = cabin;
 
   return (
@@ -18,6 +22,9 @@ function ReservationForm({ cabin }) {
           <p>{user.name}</p>
         </div> */}
       </div>
+      <p>
+        {String(range.from)} to {String(range.to)}
+      </p>
 
       <form className="flex flex-col gap-5 bg-primary-900 px-16 py-10 text-lg">
         <div className="space-y-2">
