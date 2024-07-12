eafe746096ae30cdfed419c63f2cf5ccb9e298c3
Terry Cheng <cyr062599@gmail.com>
2024-07-12 17:15:12 +0930

    refactor: Update ReservationForm to include createReservation function and handle form submission

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -28,6 +28,31 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
+export async function createReservation(bookingData, formData) {
+  const session = await auth();
+  if (!session)
+    throw new Error("You must be logged in to create a reservation");
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+
+  if (error) throw new Error("Booking could not be created");
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+  redirect("/cabins/thankyou");
+}
+
 export async function updateReservation(formData) {
   const bookingId = Number(formData.get("bookingId"));
 
